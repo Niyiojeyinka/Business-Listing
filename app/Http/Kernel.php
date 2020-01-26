f56f18a40ea39289f8c9049589e54570fb7c2901
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-01-26 19:56:21 +0100

    added proper comment to reg in kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -61,7 +61,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        //auth middlewares
+        //admin auth middlewares
         'auth.admin' => \App\Http\Middleware\AuthAdmin::class,
     ];
 
