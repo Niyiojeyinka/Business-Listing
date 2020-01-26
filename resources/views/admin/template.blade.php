3baeb05c0bee6c731b429aededbd126bbdca704a
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-01-26 20:53:30 +0100

    added w3css remote link

--- resources/views/admin/template.blade.php
+++ resources/views/admin/template.blade.php
@@ -8,7 +8,9 @@
   <meta name="description" content="A Classic Business Directory Service"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
   <link rel="stylesheet" href="{{asset('css/w3.css')}}">
-  <!--<script src="https://use.fontawesome.com/16da2feddb.js"></script>-->
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+  <script src="https://use.fontawesome.com/16da2feddb.js"></script>
   <!---editor dependency plus jquery-->  
   <link rel="stylesheet"  href="{{asset('editor/bootstrap3.3.5/css/bootstrap.css')}}">
   <script type="text/javascript" src="{{asset('editor/js/jquery.js')}}">
--- resources/views/public/common/template.blade.php
+++ resources/views/public/common/template.blade.php
@@ -8,6 +8,8 @@
   <meta name="description" content="A Classic Business Directory Service"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
   <link rel="stylesheet" href="{{asset('css/w3.css')}}">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
 <style>
     a {
         text-decoration: none;
