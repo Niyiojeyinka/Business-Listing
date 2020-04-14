bc05494259a8d68744c47a3db2d4e579ed5faef6
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-14 11:07:21 +0100

    removed unused method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,10 +23,7 @@ class HomeController extends Controller
      *
      * @return \Illuminate\Contracts\Support\Renderable
      */
-    public function index()
-    {
-        return view('home');
-    }
+    
     public function homepage()
     {
       $data['title']='A Classic Business Directory Service';
