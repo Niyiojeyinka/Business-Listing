4c0bfd20571107e2e888cfd2612c5c9d426da6c1
niyiojeyinka <olaniyiojeyinka@gmail.com>
2020-04-16 15:23:34 +0100

    WIP- worked on the business page

--- resources/views/public/business_view.blade.php
+++ resources/views/public/business_view.blade.php
@@ -5,34 +5,34 @@
 <span class="w3-xxlarge w3-serif">{{$business['name']}}</span><br>
 	
 	<img src="{{asset('images/'.json_decode($business['feature_image'])[0])}}" class="w3-image" style="max-width: 70%;" />
+
 	<div class="w3-row">
-<div class=" w3-col l9 m9 s12 w3-padding-jumbo w3-center w3-margin" >
-	<span>Contact Box</span>
-<span class="w3-left">	<span  class="fa fa-phone w3-large w3-text-teal w3-margin-right"></span>{{$business['phone']}}</span>
-<span>	<span  class="fa fa-phone fa-envelope w3-large w3-text-teal w3-margin-right"></span>{{$business['email']}}</span>
-<span class='w3-right'>	<span  class="fa fa-envelope w3-large w3-text-teal w3-margin-right"></span>{{$business['website']}}</span>
+<div class=" w3-col l9 m9 s12 w3-padding w3-center" >
+	<span>Contact Box</span><br>
+<span class="w3-left w3-margin">	<span  class="fa fa-phone w3-large w3-text-teal w3-margin-right"></span>{{$business['phone']}}</span>
+<span class="w3-center w3-margin">	<span  class="fa fa-phone fa-envelope w3-large w3-text-teal w3-margin-right"></span>{{$business['email']}}</span>
+<span class='w3-right w3-margin'>	<span  class="fa fa-envelope w3-large w3-text-teal w3-margin-right"></span>{{$business['website']}}</span>
 
 </div>
 
-<div class="w3-col l3 m3 s12">
+<div class="w3-col l3 m3 s12 w3-padding-large">
+<span>Reviews</span><br>
 
 	<i class="fa fa-star w3-medium w3-text-yellow w3-margin-right"></i>
 	<i class="fa fa-star w3-medium w3-text-yellow w3-margin-right"></i>
 	<i class="fa fa-star w3-medium w3-text-yellow w3-margin-right"></i>
 	<i class="fa fa-star w3-medium w3-text-yellow w3-margin-right"></i>
 	<i class="fa fa-star-half-empty w3-medium w3-text-yellow w3-margin-right"></i>
+	<span class="w3-text-yellow">(56)</span>
 
 		
 
 	</div>
 </div>
-<i class="fa fa-map-marker w3-jumbo w3-text-purple w3-margin-right"></i>
+<i class="fa fa-map-marker w3-xlarge w3-text-pink w3-margin-right"></i>
 {{$business['address']}}
 		<br>
-		<span class="w3-large">Categories</span><br>
-		@foreach($business['categories'] as $category )
-		<span class="w3-text-teal w3-margin w3-tag">{{$category['name']}}</span>
-		@endforeach
+		
 	<div class="w3-container w3-padding">
 		<?=$business['description']?>
 	</div>
